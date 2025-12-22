d0d99c40b9d6c7904b5ab31bd4ffbe2378bcbd11
Sanfix <pochta>
2025-12-22 23:21:56 +0300

    Add logic (need add more and fix old)

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,9 +1,29 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System;
 
 namespace Calculator;
 
 public partial class MainWindow : Window
 {
+
+    public void Add(object button, RoutedEventArgs e){
+        String content = (String)((Button)button).Content;
+        if (content == "CE"){
+            Operations.Text = "";
+        } else if (content == "+" || content == "-" || content == "x" || content == "÷" || content == "%"){
+            if (Operations.Text != "" && Operations.Text[Operations.Text.Length - 1] != '\x20'){
+                Operations.Text += "\x20";
+                Operations.Text += content;
+                Operations.Text += "\x20";
+            }
+        } else if (content == "="){
+
+        } else {
+            Operations.Text += content;
+        }
+    }
+
     public MainWindow()
     {
         InitializeComponent();
